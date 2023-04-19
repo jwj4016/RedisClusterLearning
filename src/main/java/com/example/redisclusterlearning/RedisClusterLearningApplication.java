3af1e1d5f371eca25f444999eb084e428a17e40d
J <jwj4016@gmail.com>
2023-04-19 16:12:52 +0900

    Initial commit

---
+++ src/main/java/com/example/redisclusterlearning/RedisClusterLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.redisclusterlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisClusterLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisClusterLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redisclusterlearning/RedisClusterLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redisclusterlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisClusterLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
